2d2dde595dc280bf5d411212c953e72d1d928d4b
Gilberto <gilbertoruiz28@gmail.com>
2022-09-02 04:02:53 -0300

    se define la cuadricula de 16 x 16

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
-let board = document.querySelector(".board")
+let board = document.querySelector(".board");
+//se realiza la cuadricula de trabajo
+board.style.gridTemplateColumns = 'repeat(16 , 1fr)';
+board.style.gridTemplaterows = 'repeat(16 , 1fr)';
